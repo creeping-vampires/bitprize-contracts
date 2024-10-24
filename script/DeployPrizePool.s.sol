f4d34d0dfbac5fc37e730f242ddc8ab21da095dc
creeping-vampires <creepingvampires.in@gmail.com>
2024-10-24 13:14:38 +0400

    add core deployments

--- script/DeployPrizePool.s.sol
+++ script/DeployPrizePool.s.sol
@@ -3,27 +3,27 @@ pragma solidity 0.8.24;
 
 import "forge-std/console2.sol";
 
-import { ScriptBase, Configuration } from "./ScriptBase.sol";
-import { SafeCast } from "openzeppelin/utils/math/SafeCast.sol";
+import {ScriptBase, Configuration} from "./ScriptBase.sol";
+import {SafeCast} from "openzeppelin/utils/math/SafeCast.sol";
 
-import { ERC20 } from "openzeppelin/token/ERC20/ERC20.sol";
-import { ud2x18 } from "prb-math/UD2x18.sol";
-import { sd1x18 } from "prb-math/SD1x18.sol";
+import {ERC20} from "openzeppelin/token/ERC20/ERC20.sol";
+import {ud2x18} from "prb-math/UD2x18.sol";
+import {sd1x18} from "prb-math/SD1x18.sol";
 
-import { IERC20 } from "openzeppelin/token/ERC20/IERC20.sol";
-import { ClaimerFactory } from "pt-v5-claimer/ClaimerFactory.sol";
-import { TpdaLiquidationPairFactory, TpdaLiquidationPair } from "pt-v5-tpda-liquidator/TpdaLiquidationPairFactory.sol";
-import { TpdaLiquidationRouter } from "pt-v5-tpda-liquidator/TpdaLiquidationRouter.sol";
-import { DrawManager } from "pt-v5-draw-manager/DrawManager.sol";
-import { RngWitnet, IWitnetRandomness } from "pt-v5-rng-witnet/RngWitnet.sol";
-import { IRng } from "pt-v5-draw-manager/interfaces/IRng.sol";
-import { PrizePool, ConstructorParams, SD59x18 } from "pt-v5-prize-pool/PrizePool.sol";
-import { TwabController } from "pt-v5-twab-controller/TwabController.sol";
-import { TwabRewards } from "pt-v5-twab-rewards/TwabRewards.sol";
-import { PrizeVaultFactory } from "pt-v5-vault/PrizeVaultFactory.sol";
-import { PrizeVault, IERC4626 } from "pt-v5-vault/PrizeVault.sol";
-import { StakingVault, IERC20 as StakingVaultIERC20 } from "pt-v5-staking-vault/StakingVault.sol";
-import { VaultBoosterFactory } from "pt-v5-vault-boost/VaultBoosterFactory.sol";
+import {IERC20} from "openzeppelin/token/ERC20/IERC20.sol";
+import {ClaimerFactory} from "pt-v5-claimer/ClaimerFactory.sol";
+import {TpdaLiquidationPairFactory, TpdaLiquidationPair} from "pt-v5-tpda-liquidator/TpdaLiquidationPairFactory.sol";
+import {TpdaLiquidationRouter} from "pt-v5-tpda-liquidator/TpdaLiquidationRouter.sol";
+import {DrawManager} from "pt-v5-draw-manager/DrawManager.sol";
+import {RngWitnet, IWitnetRandomness} from "pt-v5-rng-witnet/RngWitnet.sol";
+import {IRng} from "pt-v5-draw-manager/interfaces/IRng.sol";
+import {PrizePool, ConstructorParams, SD59x18} from "pt-v5-prize-pool/PrizePool.sol";
+import {TwabController} from "pt-v5-twab-controller/TwabController.sol";
+import {TwabRewards} from "pt-v5-twab-rewards/TwabRewards.sol";
+import {PrizeVaultFactory} from "pt-v5-vault/PrizeVaultFactory.sol";
+import {PrizeVault, IERC4626} from "pt-v5-vault/PrizeVault.sol";
+import {StakingVault, IERC20 as StakingVaultIERC20} from "pt-v5-staking-vault/StakingVault.sol";
+import {VaultBoosterFactory} from "pt-v5-vault-boost/VaultBoosterFactory.sol";
 
 string constant addressBookPath = "config/addressBook.txt";
 
@@ -64,10 +64,15 @@ contract DeployPrizePool is ScriptBase {
     function run() public virtual {
         vm.startBroadcast();
 
-        if (keccak256(bytes(config.rngType)) == keccak256("witnet-randomness-v2")) {
+        if (
+            keccak256(bytes(config.rngType)) ==
+            keccak256("witnet-randomness-v2")
+        ) {
             /// WITNET
             standardizedRng = new RngWitnet(IWitnetRandomness(config.rng));
-        } else if (keccak256(bytes(config.rngType)) == keccak256("standardized")){
+        } else if (
+            keccak256(bytes(config.rngType)) == keccak256("standardized")
+        ) {
             /// STANDARDIZED
             standardizedRng = IRng(config.rng);
         } else {
@@ -107,13 +112,17 @@ contract DeployPrizePool is ScriptBase {
         twabController = new TwabController(
             config.twabPeriodLength,
             (firstDrawStartsAt -
-                ((firstDrawStartsAt - block.timestamp) / config.twabPeriodLength + 1) * config.twabPeriodLength
-            ).toUint32()
+                ((firstDrawStartsAt - block.timestamp) /
+                    config.twabPeriodLength +
+                    1) *
+                config.twabPeriodLength).toUint32()
         );
         new TwabRewards(twabController);
 
         liquidationPairFactory = new TpdaLiquidationPairFactory();
-        tpdaLiquidationRouter = new TpdaLiquidationRouter(liquidationPairFactory);
+        tpdaLiquidationRouter = new TpdaLiquidationRouter(
+            liquidationPairFactory
+        );
         prizeVaultFactory = new PrizeVaultFactory();
 
         prizePool = new PrizePool(
@@ -134,13 +143,19 @@ contract DeployPrizePool is ScriptBase {
         );
 
         claimerFactory = new ClaimerFactory();
-        claimer = address(claimerFactory.createClaimer(
-            prizePool,
-            config.claimerTimeToReachMaxFee,
-            config.claimerMaxFeePercent
-        ));
+        claimer = address(
+            claimerFactory.createClaimer(
+                prizePool,
+                config.claimerTimeToReachMaxFee,
+                config.claimerMaxFeePercent
+            )
+        );
 
-        stakingVault = new StakingVault(config.stakingVaultName, config.stakingVaultSymbol, StakingVaultIERC20(config.stakedAsset));
+        stakingVault = new StakingVault(
+            config.stakingVaultName,
+            config.stakingVaultSymbol,
+            StakingVaultIERC20(config.stakedAsset)
+        );
         stakingPrizeVault = prizeVaultFactory.deployVault(
             config.stakingPrizeVaultName,
             config.stakingPrizeVaultSymbol,
--- script/ts/generateDeployments.ts
+++ script/ts/generateDeployments.ts
@@ -1,11 +1,10 @@
-import {
-  writeFiles
-} from "./generateContractList";
+import { writeFiles } from "./generateContractList";
 
 writeFiles(31337, "localhost");
 writeFiles(10, "optimism");
 writeFiles(8453, "base");
-writeFiles(42161, "arbitrum")
+writeFiles(42161, "arbitrum");
 writeFiles(1, "ethereum");
 writeFiles(534352, "scroll");
 writeFiles(100, "gnosis");
+writeFiles(1116, "core");
